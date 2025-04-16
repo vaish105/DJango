51670b041d885dc98a0f9b126cdef737d1a78650
vaish105 <vaishnavivjadhav04@gmail.com>
2025-04-16 17:06:33 +0530

    Adding

--- Flipkart/Electronics/views.py
+++ Flipkart/Electronics/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def home(request):
+    prod= Product.objects.all()
+    return render(request, 'Electronics/home.html', {'prod': prod})
